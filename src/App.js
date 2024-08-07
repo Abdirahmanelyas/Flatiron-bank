f02c6693245096ee7472015abb8ad43064a4a621
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-08-08 02:16:37 +0300

    transaction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import AccountContainer from "./component/AccountContainer";
+import AccountContainer from "./components/AccountContainer";
 import './App.css';
 function App() {
   return (
-    
+    <div className="App">
+      <div className="app-icon">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+    </div>
   );
 }
 
